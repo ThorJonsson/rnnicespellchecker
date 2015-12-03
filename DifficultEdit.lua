6aedc814b78d69e401dd6dd27a767299c8f6a39a
Þorsteinn Hjörtur Jónsson <thj92@hi.is>
2015-12-03 02:03:17 +0000

    debugging prints removed, we really need to learn how to use a lua debugger...this is a functional version although it can be improved it is probably good enough for a proof of concept. We should also look into vector representations they seem to be the way to go

--- DifficultEdit.lua
+++ DifficultEdit.lua
@@ -32,118 +32,105 @@ function charVec(m)
         return str
 end
 
-function CompStrings(filapath)
+function CompStrings(filepath)
     require 'lfs'
     require 'csvigo'
-    for file in lfs.dir("/home/thj92/Haust2015/Reiknigreind/char-rnn/data/LevenshteinTwoCol/") do
-    m = csvigo.load{path='/home/thj92/Haust2015/Reiknigreind/char-rnn/data/LevenshteinTwoCol/'..file ,mode='raw'}
-    end
-        csvigo.save{data=m,path='/home/thj92/Haust2015/Reiknigreind/char-rnn/data/LevenshteinTwoCol/'..'NewRep'..file}
-   l =2
-   while l <= #m do
-       if string.len(m[l][1])==0 and string.len(m[l][2])==0 then l=l+1 end 
-        wrong = m[l][1] 
-        correct = m[l][2]
-        
-        --Convert string arguments to arrays of ints
-        if type( wrong ) == "string" then
-            sTab = { string.byte( wrong, 1, #wrong ) }
+    for file in lfs.dir(filepath) do
+        m = csvigo.load{path=filepath..file ,mode='raw'}
+        l =2
+         while l <= #m do
+             if string.len(m[l][1])==0 and string.len(m[l][2])==0 then l=l+1 end 
+              wrong = m[l][1] 
+              correct = m[l][2]
+              
+              --Convert string arguments to arrays of ints
+              if type( wrong ) == "string" then
+                  sTab = { string.byte( wrong, 1, #wrong ) }
+              end
+
+              if type( correct ) == "string" then
+                 tTab = { string.byte( correct, 1, #correct ) }
+              end
+              if strLen(correct) <= strLen(wrong) then
+                      LongerW = wrong
+                      LongerWTab = sTab
+                      ShorterW = correct
+                      ShorterWTab = tTab
+              else
+                      LongerW = correct
+                      LongerWTab = tTab
+                      ShorterW = wrong
+                      ShorterWTab = sTab
+              end
+              longerCorr = LongerWTab
+              shorterCorr = {}
+              lastPos = 0
+              startPos = 1
+              n = strLen(ShorterW)
+              k = 1
+              j = 0
+              i = 1
+              sumj = 0
+              -- We must go through each byt in shorterW
+              while i <= #ShorterWTab do
+                  -- k will run through all replacements so it will go from 1 to n
+                  if ShorterWTab[i] == 195 then
+                          j = 1
+                          sumj = sumj + j
+                  else
+                          j = 0 
+                  end
+                  -- always a single char, we need j=i+1 if it is icelandic
+                  -- if it is icelandic then necessarily ShorterWTab[i] == 195
+                  -- consequently we need to jump over the next bit
+                  currChar = string.sub(ShorterW,i,i+j)
+                  -- now we look for this char in our string
+                  if currChar == '%' then currChar = '%%' end
+                  if currChar == '(' then currChar = '%(' end
+                  if currChar == '[' then currChar = '%[' end
+                  startPos = string.find(string.sub(LongerW,lastPos+1,#LongerW),currChar)
+                  -- if startPos is nil then our char can not be found in the longer word
+                  if startPos == nil then
+                          shorterCorr[k] = currChar
+                  -- if the letter is icelandic j = 1 and we jump over the extra bytes from the unicode char
+                          k = k + 1
+                  else
+                      startPos = startPos + lastPos
+                  -- if the letter is found we simple put an asterisk to confirm happiness
+                          shorterCorr[k] = ' '
+                  -- if we find the startPos we must make sure that the longerCorr indices below that one and the lastpos contain proper letters
+                          longerCorr[startPos] = ' '
+                          if j>0 then 
+                              longerCorr[startPos+j] = '$' 
+                          end
+                          for r=1,startPos do
+                          end
+                          lastPos = startPos+j
+                          k = k+ 1
+                  end
+
+                  i = i+j+1
+                  -- if it is not nil then we shall put * in the longercorr vector for the position that corresponds to it,
+                  -- and * as well in the shortercorr vector.
+                  -- Then we will look to see whether it the position in front of it has a star or not
+                  -- So we need to keep track of the last position
+                  
+
+             end
+             if strLen(correct) >= strLen(wrong) then
+
+              table.insert(m[i],charVec(shorterCorr))
+              table.insert(m[i],charVec(longerCorr))
+             else
+              table.insert(m[i],charVec(longerCorr))
+              table.insert(m[i],charVec(shorterCorr))
+             end
+              l = l+1
+         end
+  
+                csvigo.save{data=m,path=filepath..'NewRep'..file}
+                end
         end
-
-        if type( correct ) == "string" then
-           tTab = { string.byte( correct, 1, #correct ) }
-        end
-        if strLen(correct) <= strLen(wrong) then
-                LongerW = wrong
-                LongerWTab = sTab
-                ShorterW = correct
-                ShorterWTab = tTab
-        else
-                LongerW = correct
-                LongerWTab = tTab
-                ShorterW = wrong
-                ShorterWTab = sTab
-        end
-        longerCorr = LongerWTab
-        shorterCorr = {}
-        lastPos = 0
-        startPos = 1
-        n = strLen(ShorterW)
-        k = 1
-        j = 0
-        i = 1
-        sumj = 0
-        -- We must go through each byt in shorterW
-        while i <= #ShorterWTab do
-            -- k will run through all replacements so it will go from 1 to n
-            if ShorterWTab[i] == 195 then
-                    j = 1
-                    sumj = sumj + j
-            else
-                    j = 0 
-            end
-            -- always a single char, we need j=i+1 if it is icelandic
-            -- if it is icelandic then necessarily ShorterWTab[i] == 195
-            -- consequently we need to jump over the next bit
-            currChar = string.sub(ShorterW,i,i+j)
-         --  print('i is :'.. i .. '      j is : '.. j)
-        --    print('currChar is   :'.. currChar) 
-            -- now we look for this char in our string
-        --    print('The string we are looking for currChar in is:    '..string.sub(LongerW,lastPos+1,#LongerW))
-            if currChar == '%' then currChar = '%%' end
-            if currChar == '(' then currChar = '%(' end
-            if currChar == '[' then currChar = '%[' end
-            if #LongerW - lastPos -1 > 4 then 
-            startPos = string.find(string.sub(LongerW,lastPos+1,#LongerW),currChar)
-            -- if startPos is nil then our char can not be found in the longer word
-            if startPos == nil then
-                    shorterCorr[k] = currChar
-          --         print('shorterCorr is : '.. charVec(shorterCorr))
-           --        print('longerCorr is : '.. charVec(longerCorr))
-            -- if the letter is icelandic j = 1 and we jump over the extra bytes from the unicode char
-                    k = k + 1
-            else
-                startPos = startPos + lastPos
-              print('The starting position found is:   ' ..startPos)
-            -- if the letter is found we simple put an asterisk to confirm happiness
-                    shorterCorr[k] = ' '
-            -- if we find the startPos we must make sure that the longerCorr indices below that one and the lastpos contain proper letters
-                    longerCorr[startPos] = ' '
-             --       print(longerCorr[startPos]..'-----'..startPos..'-----'..lastPos..'-----'..#longerCorr)
-                    if j>0 then 
-                        longerCorr[startPos+j] = '$' 
-                    end
-                    for r=1,startPos do
-              --          print(longerCorr[r])
-                    end
-                    lastPos = startPos+j
-               --  print('shorterCorr is : '.. charVec(shorterCorr))
-                --   print('longerCorr is : '.. charVec(longerCorr))
-
-                    k = k+ 1
-            end
-
-            i = i+j+1
-            -- if it is not nil then we shall put * in the longercorr vector for the position that corresponds to it,
-            -- and * as well in the shortercorr vector.
-            -- Then we will look to see whether it the position in front of it has a star or not
-            -- So we need to keep track of the last position
-            
-
-       end
-       if strLen(correct) >= strLen(wrong) then
-
-        table.insert(m[i],charVec(shorterCorr),charVec(longerCorr))
-       --print(l..'  ins:'..charVec(shorterCorr)..'  |  del:'..charVec(longerCorr))
-       else
-
-        table.insert(m[i],charVec(longerCorr),charVec(shorterCorr))
-       --print(l..'  ins:'..charVec(longerCorr)..'  |  del:'..charVec(shorterCorr))
-       end
-        l = l+1
-       end
-end
-
 
 
         
